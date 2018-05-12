bfbcf1796d9aebeca6a0a7c5599b9ab0251c73c8
Francisco <TheWebs@users.noreply.github.com>
2018-05-12 14:31:25 +0100

    Add files via upload

---
+++ scripts/funcoes.js
@@ -0,0 +1,8 @@
+﻿function loadImages()
+{
+    var element = document.getElementById("imagens");
+    for(var a = 1; a <=9; a++)
+    {
+        element.insertAdjacentHTML("beforeend", "<img class='showcase' src='/images/" + a.toString() + ".jpg' width=480 height=270/>");
+    }
+}
