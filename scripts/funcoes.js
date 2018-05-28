f5ec645581e725f27de3ccb6509bac759008fdb8
Francisco <TheWebs@users.noreply.github.com>
2018-05-28 19:12:48 +0100

    Update funcoes.js

--- scripts/funcoes.js
+++ scripts/funcoes.js
@@ -8,7 +8,7 @@ function loadImages()
     var element = document.getElementById("imagens");
     for(var a = 1; a <=9; a++)
     {
-        element.insertAdjacentHTML("beforeend", "<img class='showcase' src='/images/" + a.toString() + ".jpg' width=480 height=270/>");
+        element.insertAdjacentHTML("beforeend", "<img class='showcase' src='images/" + a.toString() + ".jpg' width=480 height=270/>");
     }
     showHome();
 }
